a5fd7101008e488476cb39d5d6e851848c754f7b
VullnetJ <v.jata24@gmail.com>
2021-08-29 23:52:03 +0300

    first commit

---
+++ Wed.js
@@ -0,0 +1,6 @@
+let populationArray = [50000, 80000, 3000, ];
+console.log(population);
+
+for ( i = 0; i <=populationArray.length; i ++ ) {
+    console.log(populationArray[i]);
+}
---
+++ javascript.js
@@ -0,0 +1,27 @@
+// function replaceImages() {
+//     let images = document.body.getElementsByTagName("img");
+//     for (let i = images.length -1; i >= 0; i --) {
+//         let image = images[i];
+//         if (image.alt) {
+//             let text = document.createTextNode(image.alt);
+//             image.parentNode.replaceChild(text, image);
+//         }
+//     }
+// }   
+
+// const square = function(x) {
+//     return x*x;
+// }; 
+// console.log(square(12)); // it calcuates x *x
+
+// var makeNoise = function() {
+//     console.log("Pling"); // it makes the pling noise
+// };
+// makeNoise(); //
+
+// const halve = function(n) {
+//     return n/2;
+// }
+// let n = 10;
+// console.log(halve(100)); // it returns 50 
+// console.log(n); // it returns 10. 
